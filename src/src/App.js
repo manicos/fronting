02e5a99b44d3cbe54ae84d5f861b26e3c5c7ee2b
Alfredo Bolio <alfredobolio@Alfredos-MacBook-Pro-2.local>
2026-01-23 16:21:09 -0600

    Adición de primer código fuente base

---
+++ src/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          HelloWorld
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
