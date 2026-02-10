40d900b870845525eb72101384a3cb67a3fc9f5d
Alfredo Bolio <alfredobolio@Alfredos-MacBook-Pro-2.local>
2026-02-09 18:09:43 -0600

    ACtualización 2

--- src/src/App.js
+++ src/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Me gusto el trailer de Mario Bros Galaxy Movie
+          Me gusto el medio tiempo del superbowl la salbo Lady Gaga y Ricardo Martinez,<br></br> aunque me caga Badbuny pero me da gusto qu elos latinos se escuchen y tengan éxito.
         </p>
       </header>
     </div>
