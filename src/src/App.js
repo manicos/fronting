3786b2bb89e081a765aa97fe3b9490f3de2cb086
Alfredo Bolio <alfredobolio@Alfredos-MacBook-Pro-2.local>
2026-03-02 17:37:28 -0600

    CI/CD con Azure Static Web Apps

--- src/src/App.js
+++ src/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Me gusto el medio tiempo del superbowl la salbo Lady Gaga y Ricardo Martinez,<br></br> aunque me caga Badbuny pero me da gusto qu elos latinos se escuchen y tengan éxito.
+          Mi primer proyecto en la nube sin gestionar infraestructura<br></br>
         </p>
       </header>
     </div>
